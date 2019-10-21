6b7f5418256fd150bab0e46ec71fdbfccc4667be
2dam <2dam@HZ341352.TartangaLH.eus>
2019-10-21 13:19:35 +0200

    SerialVersionUID update

--- src/clases/Mensaje.java
+++ src/clases/Mensaje.java
@@ -12,6 +12,7 @@ import java.io.Serializable;
  * @author 2dam
  */
 public class Mensaje implements Serializable{
+    private static final long serialVersionUID = 1L;
     private int opc;
     private Object data;
 
