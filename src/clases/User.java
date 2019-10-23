0604b5730d77633a4cc4cfd892fa53212832278c
2dam <2dam@HZ341352.TartangaLH.eus>
2019-10-23 10:36:27 +0200

    Version 23/10/219 Lado server terminado.

--- src/clases/User.java
+++ src/clases/User.java
@@ -5,13 +5,15 @@
  */
 package clases;
 
+import java.io.Serializable;
 import java.util.Date;
 
 /**
  *
  * @author 2dam
  */
-public class User {
+public class User implements Serializable{
+    private static final long serialVersionUID = 1L;
     private int id;
     private String login;
     private String email;
