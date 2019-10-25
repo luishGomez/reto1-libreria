0345484924778ed1ff883a18f9c148573b22c0fa
2dam <2dam@HZ341352.TartangaLH.eus>
2019-10-25 13:04:52 +0200

    retoques

---
+++ src/exceptions/EsperaCompletaException.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptions;
+
+/**
+ *
+ * @author 2dam
+ */
+public class EsperaCompletaException extends Exception{
+    public EsperaCompletaException(String message){
+        super(message);
+    }
+    public EsperaCompletaException(){
+    }
+    
+}
